ab093d0855dbbe12f1bfe1abd3a2172c66102300
Leisvan Cordero <rathmA@LIVE.COM>
2020-03-05 23:58:23 -0500

    Test Data filled

--- SettingsUWP/Services/TestDataService.cs
+++ SettingsUWP/Services/TestDataService.cs
@@ -26,19 +26,98 @@ namespace SettingsUWP.Services
                 Title = "Devices",
                 Description = "Bluetooth, printers, mouse",
             };
-            devices.Groups.Add(new SettingSubtitle { Text = "Bluetooth & other devices", IconGlyph = "" });
-
-            var devices2 = new SettingGroup
+            
+            var phone = new SettingGroup
             {
-                Kind = SettingGroupKind.Devices,
-                Title = "Devices 2",
-                Description = "Bluetooth, printers, mouse",
+                Kind = SettingGroupKind.Phone,
+                Title = "Phone",
+                Description = "Link your Android, iPhone",
             };
-            devices2.Groups.Add(new SettingSubtitle { Text = "Bluetooth & other devices", IconGlyph = "" });
+
+            var network = new SettingGroup
+            {
+                Kind = SettingGroupKind.NetworkAndInternet,
+                Title = "Network & Internet",
+                Description = "Wi-Fi, airplane mode, VPN",
+            };
+
+            var personalization = new SettingGroup
+            {
+                Kind = SettingGroupKind.Personalization,
+                Title = "Personalization",
+                Description = "Background, lock screen, colors",
+            };
+
+            var apps = new SettingGroup
+            {
+                Kind = SettingGroupKind.Apps,
+                Title = "Apps",
+                Description = "Uninstall, defaults, optional features",
+            };
+
+            var accounts = new SettingGroup
+            {
+                Kind = SettingGroupKind.Account,
+                Title = "Accounts",
+                Description = "Your accounts, email, sync, work, family",
+            };
+
+            var time = new SettingGroup
+            {
+                Kind = SettingGroupKind.TimeAndLanguage,
+                Title = "Time & Language",
+                Description = "Speech, region, date",
+            };
+
+            var gaming = new SettingGroup
+            {
+                Kind = SettingGroupKind.Gaming,
+                Title = "Gaming",
+                Description = "Game bar, captues, broadcasting, game Mode",
+            };
+
+            var easeAccess = new SettingGroup
+            {
+                Kind = SettingGroupKind.EaseOfAccess,
+                Title = "Ease of Access",
+                Description = "Narrator, magnifier, high contrast",
+            };
+
+            var cortanaAndSearch = new SettingGroup
+            {
+                Kind = SettingGroupKind.CortanaAndSearch,
+                Title = "Cortana & Search",
+                Description = "Cortana language, permissions, notifications, find my files",
+            };
+
+            var privacy = new SettingGroup
+            {
+                Kind = SettingGroupKind.Privacy,
+                Title = "Privacy",
+                Description = "Location, camera, microphone",
+            };
+
+            var update = new SettingGroup
+            {
+                Kind = SettingGroupKind.UpdateAndSecurity,
+                Title = "Update & Security",
+                Description = "Windows update, recovery, backup",
+            };
+
 
             groups.Add(system);
             groups.Add(devices);
-            groups.Add(devices2);
+            groups.Add(phone);
+            groups.Add(network);
+            groups.Add(personalization);
+            groups.Add(apps);
+            groups.Add(accounts);
+            groups.Add(time);
+            groups.Add(gaming);
+            groups.Add(easeAccess);
+            groups.Add(cortanaAndSearch);
+            groups.Add(privacy);
+            groups.Add(update);
 
             return groups;
         }
