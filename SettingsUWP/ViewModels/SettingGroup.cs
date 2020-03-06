29f68ac943d8dc264504f2cd7d5bd2283792f5d4
Leisvan Cordero <rathmA@LIVE.COM>
2020-03-05 21:37:46 -0500

    Adding DataModels and Test assets

---
+++ SettingsUWP/ViewModels/SettingGroup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsUWP.ViewModels
+{
+    public class SettingGroup
+    {
+        public SettingGroupKind Kind { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<SettingSubtitle> Groups { get; set; } = new List<SettingSubtitle>();
+    }
+
+    public class SettingSubtitle
+    {
+        public string Text { get; set; }
+        public string IconGlyph { get; set; }
+    }
+
+    public enum SettingGroupKind
+    {
+        System,
+        Devices,
+        Phone,
+        NetworkAndInternet,
+        Personalization,
+        Apps,
+        Account,
+        TimeAndLanguage,
+        Gaming,
+        EaseOfAccess,
+        CortanaAndSearch,
+        Privacy,
+        UpdateAndSecurity,
+    }
+}
